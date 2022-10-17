aec2e35344c7f173d7941328218f12b836f289ee
Xsuto <n>
2022-10-17 06:38:45 +0200

    Auto accept 3.0

--- accept.py
+++ accept.py
@@ -17,6 +17,7 @@ from pytimedinput import timedInput
 from localStoragePy import localStoragePy
 from PIL import Image
 from LolChampions import CHAMPIONS
+from cv2 import Mat
 
 WINDOW_NAME = "League of Legends"
 
@@ -32,7 +33,7 @@ CONFIRM_BUTTON_X = 1200
 CONFIRM_BUTTON_Y = 1130
 
 
-def screenshot_of_app_in_background(window_name):
+def screenshot_of_app_in_background(window_name: str):
     hwnd = win32gui.FindWindow(None, window_name)
     x, y, width, height = get_lol_position()
     hwndDC = win32gui.GetWindowDC(hwnd)
@@ -61,7 +62,7 @@ def get_lol_position():
     return GetWindowRect(window_handle)
 
 
-def match_template(Image, template, threshold=0.9):
+def match_template(Image: Mat, template: Mat, threshold=0.9):
     res = cv2.matchTemplate(Image, template, cv2.TM_CCORR_NORMED)
     h, w = template.shape
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
@@ -71,20 +72,20 @@ def match_template(Image, template, threshold=0.9):
     return False, [], 0, 0
 
 
-def mouseClick(x, y):
+def mouseClick(x: int, y: int):
     mouseController = mouse.Controller()
     mouseController.position = (x, y)
     mouseController.press(Button.left)
     mouseController.release(Button.left)
 
 
-def typeAt(text, x, y):
+def typeAt(text: str, x: int, y: int):
     keyboard = Controller()
     mouseClick(x, y)
     keyboard.type(text)
 
 
-def find_template_on_image(image, template, shouldClick=False, callback=()):
+def find_template_on_image(image: Mat, template: Mat, shouldClick=False, callback=()):
     found, loc, w, h = match_template(image, template)
     callback
     if found:
@@ -102,7 +103,8 @@ def screenshot():
         return screenshot
 
 
-def champion_spell_corection(champion):
+def champion_spell_corection(champion: str):
+    champion = champion.lower()
     if not len(champion):
         return ""
     best = champion
@@ -122,7 +124,7 @@ def get_local_storage():
     return localStoragePy("xsuto.accept-lol.app", "json")
 
 
-def accept_game(acceptBtn, inLobby, dodge_message):
+def accept_game(acceptBtn: Mat, inLobby: Mat, dodge_message: Mat):
     did_accept = False
     print("Waiting for game stage")
     while True:
@@ -142,14 +144,14 @@ def accept_game(acceptBtn, inLobby, dodge_message):
         sleep(1)
 
 
-def select_champion(champion, x, y):
+def select_champion(champion: str, x: int, y: int):
     typeAt(champion, x + SEARCH_CHAMPION_INPUT_X, y + SEARCH_CHAMPION_INPUT_Y)
     sleep(0.5)
     mouseClick(x + SELETED_CHAMP_X, y + SELETED_CHAMP_Y)
     mouseClick(x + SELETED_CHAMP_X, y + SELETED_CHAMP_Y)
 
 
-def focus_window(window_name):
+def focus_window(window_name: str):
     try:
         window_handle = FindWindow(None, window_name)
         # SetForegroundWindow weird error sometimes this function crashes program for no reason
@@ -159,33 +161,76 @@ def focus_window(window_name):
         pass
 
 
-def look_for_a_game(findMatch):
+def look_for_a_game(findMatch: Mat):
     focus_window(WINDOW_NAME)
+    sleep(0.5)
     find_template_on_image(screenshot(), findMatch, True)
+
+
+def float_or_0(str: str):
+    try:
+        return float(str)
+    except:
+        return 0
+
+
+def sleep_with_timer(secs: int):
+    i = 0
+    while i < secs:
+        print("\r" + f"{int(secs - i)} seconds left", end="")
+        i += 1
+        sleep(1)
 
 
 def get_args():
     local_storage = get_local_storage()
+    autoPickAndBan = bool(local_storage.getItem("PickAndBan") or False)
+    dodge_time = 0
     banChamp = str(local_storage.getItem("banChamp") or "")
     pickChamp = str(local_storage.getItem("pickChamp") or "")
     try:
         banChamp = sys.argv[1]
         pickChamp = sys.argv[2]
     except:
-        text, did_timeout = timedInput(f"Ban Champion (previously {banChamp}): ")
-        if not did_timeout and text:
-            banChamp = text
-        text, did_timeout = timedInput(f"Pick Champion (previously {pickChamp}): ")
-        if not did_timeout and text:
-            pickChamp = text
         text, did_timeout = timedInput(
-            "Dodge timer in second (if 0 then press enter): "
+            f"Auto pick and ban champion (Y/N) (previously {autoPickAndBan}): "
+        )
+
+        if not did_timeout and text:
+            autoPickAndBan = text.lower() == "y"
+
+        text, did_timeout = timedInput(
+            "Dodge timer in minutes (if 0 then press enter): "
         )
         if not did_timeout and text:
-            sleep(float(text) * 60)
+            dodge_time = float_or_0(text) * 60
+
+        if not autoPickAndBan:
+            local_storage.setItem("banChamp", "")
+            local_storage.setItem("pickChamp", "")
+            return ("", "")
+
+        text, did_timeout = timedInput(f"Ban Champion (previously {banChamp}): ")
+        if text == "none":
+            banChamp = ""
+        elif not did_timeout and text:
+            banChamp = text
+
+        text, did_timeout = timedInput(f"Pick Champion (previously {pickChamp}): ")
+        if text == "none":
+            pickChamp = ""
+        elif not did_timeout and text:
+            pickChamp = text
+
+    sleep_with_timer(dodge_time)
+    local_storage.setItem("PickAndBan", autoPickAndBan)
     local_storage.setItem("pickChamp", champion_spell_corection(pickChamp))
     local_storage.setItem("banChamp", champion_spell_corection(banChamp))
     return (champion_spell_corection(banChamp), champion_spell_corection(pickChamp))
+
+
+def load_img_from_string(str):
+    return cv2.imdecode(np.fromstring(str, np.uint8, sep=","), -1)
 
 
 def main():
@@ -199,19 +244,12 @@ def main():
     print(banChamp, pickChamp)
 
     # Load templates
-    in_lobby = cv2.imdecode(
-        np.fromstring(string_encoded_lobby_template, np.uint8, sep=","), -1
-    )
-    find_match = cv2.imdecode(
-        np.fromstring(string_encoded_find_match_template, np.uint8, sep=","), -1
-    )
-    accept_btn = cv2.imdecode(
-        np.fromstring(string_encoded_accept_btn_template, np.uint8, sep=","), -1
-    )
-    lock_in = cv2.imdecode(np.fromstring(string_encoded_lock_in, np.uint8, sep=","), -1)
-    dodge_message = cv2.imdecode(
-        np.fromstring(string_encoded_dodge_message, np.uint8, sep=","), -1
-    )
+    in_lobby = load_img_from_string(string_encoded_lobby_template)
+    find_match = load_img_from_string(string_encoded_find_match_template)
+    accept_btn = load_img_from_string(string_encoded_accept_btn_template)
+    lock_in = load_img_from_string(string_encoded_lock_in)
+    ban_champion_stage = load_img_from_string(string_encoded_ban_champion)
+    dodge_message = load_img_from_string(string_encoded_dodge_message)
 
     # Find game
     look_for_a_game(find_match)
@@ -220,28 +258,33 @@ def main():
     accept_game(accept_btn, in_lobby, dodge_message)
 
     # Selecet champion that you want to play
-    if not pickChamp or not banChamp:
-        return
 
-    print("Select champion stage")
-    sleep(5)
-    x, y, w, h = get_lol_position()
-    focus_window(WINDOW_NAME)
-    select_champion(pickChamp, x, y)
+    if pickChamp:
+        print("Select champion stage")
+        sleep(5)
+        x, y, w, h = get_lol_position()
+        focus_window(WINDOW_NAME)
+        select_champion(pickChamp, x, y)
 
     # BAN CHAMP
-    print("Ban champion stage")
-    sleep(11)
-    x, y, w, h = get_lol_position()
-    focus_window(WINDOW_NAME)
-    select_champion(banChamp, x, y)
-    sleep(0.5)
-    mouseClick(x + CONFIRM_BUTTON_X, y + CONFIRM_BUTTON_Y)
+    if banChamp:
+        print("Ban champion stage")
+        while not find_template_on_image(
+            screenshot_of_app_in_background(WINDOW_NAME), ban_champion_stage
+        ):
+            sleep(1)
+        sleep(2)
+        x, y, w, h = get_lol_position()
+        focus_window(WINDOW_NAME)
+        select_champion(banChamp, x, y)
+        sleep(0.5)
+        mouseClick(x + CONFIRM_BUTTON_X, y + CONFIRM_BUTTON_Y)
 
     # PICK CHAMP
-    print("Pick Champion stage")
-    while not find_template_on_image(screenshot(), lock_in, True):
-        sleep(3)
+    if pickChamp:
+        print("Pick Champion stage")
+        while not find_template_on_image(screenshot(), lock_in, True):
+            sleep(3)
 
 
 main()
